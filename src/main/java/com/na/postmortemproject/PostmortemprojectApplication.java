c46ca4a8f69f427ac0bda44e4a722512767ba617
Armando Valera <amvdevlab@gmail.com>
2025-12-01 17:10:47 -0500

    INITIAL PROJECT SETUP W/ SPRING BOOT AND JAVA

---
+++ src/main/java/com/na/postmortemproject/PostmortemprojectApplication.java
@@ -0,0 +1,13 @@
+package com.na.postmortemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmortemprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmortemprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/na/postmortemproject/PostmortemprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.na.postmortemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmortemprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
