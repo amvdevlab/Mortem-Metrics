d1b23dd9fafb06a77d9e9e2973893c13f740050b
Armando Valera <amvdevlab@gmail.com>
2026-01-07 17:03:48 -0500

    fix: test automation
    fixed application-test.properties
    removed parser directory from christmas_sprint
    configured PostmortemprojectApplicationTests

--- src/test/java/com/na/postmortemproject/PostmortemprojectApplicationTests.java
+++ src/test/java/com/na/postmortemproject/PostmortemprojectApplicationTests.java
@@ -2,8 +2,11 @@ package com.na.postmortemproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PostmortemprojectApplicationTests {
 
 	@Test
