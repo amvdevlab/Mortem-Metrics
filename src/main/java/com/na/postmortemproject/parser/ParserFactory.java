95c1018785776af8734517e2984425f10d241485
Armando Valera <amvdevlab@gmail.com>
2026-01-15 22:02:57 -0500

    build: start the parsing architecture for csv
    Built the FileParser logic and interface.
    Built the ParserFactory for instance direction.
    Built CsvParser initial logic to parse from csv
    format to text in bytes.

--- src/main/java/com/na/postmortemproject/parser/ParserFactory.java
+++ src/main/java/com/na/postmortemproject/parser/ParserFactory.java
@@ -1,5 +1,39 @@
 package com.na.postmortemproject.parser;
 
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
 public class ParserFactory {
 
+
+  public FileParser getParser (String contentType) {
+    
+    if (contentType == null) {
+      throw new IllegalArgumentException("Content type cannot be null");
+    }
+    
+    // debug input
+    log.debug("Getting parser for content type: {}", contentType);
+
+    String baseType = contentType.split(";")[0].trim().toLowerCase();
+
+    FileParser parser =  switch (baseType) {
+
+      case "text/csv", "application/csv" -> new CsvParser();
+
+
+
+      default -> throw new UnsupportedOperationException(
+        "Unsupported file type: " + contentType + ". Currently only CSV is supported"
+      );
+    };
+    
+    // debug output
+    log.debug("Returning parser: {}", parser.getClass().getSimpleName());
+    
+    return parser;
+  }
 }
