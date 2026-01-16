edf999beb38690cca9c0eb511cc2f977704135a7
Armando Valera <amvdevlab@gmail.com>
2026-01-15 22:02:57 -0500

    build: start the parsing architecture for csv
    Built the FileParser logic and interface.
    Built the ParserFactory for instance direction.
    Built CsvParser initial logic to parse from csv
    format to text in bytes.

--- src/main/java/com/na/postmortemproject/parser/CsvParser.java
+++ src/main/java/com/na/postmortemproject/parser/CsvParser.java
@@ -1,5 +1,25 @@
 package com.na.postmortemproject.parser;
 
-public class CsvParser {
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
+public class CsvParser implements FileParser {
+  @Override
+  public String extractText(InputStream input) throws IOException {
+
+    if (input == null) {
+      throw new IllegalArgumentException("Input stream cannot be null");
+    }
+
+    byte[] bytes = input.readAllBytes();
+
+    if (bytes.length == 0) {
+      throw new IllegalArgumentException("File is empty");
+    }
+
+    String text = new String(bytes, StandardCharsets.UTF_8);
+    
+    return text;
+  }
 }
--- src/main/java/com/na/postmortemproject/parser/FileParser.java
+++ src/main/java/com/na/postmortemproject/parser/FileParser.java
@@ -1,5 +1,7 @@
 package com.na.postmortemproject.parser;
 
+import java.io.IOException;
+import java.io.InputStream;
 public interface FileParser {
-
+  String extractText(InputStream input) throws IOException;
 }
