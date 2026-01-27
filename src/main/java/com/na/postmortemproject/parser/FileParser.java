59f90568ca111bbd33abe95850f45e977c411a8a
Armando Valera <amvdevlab@gmail.com>
2026-01-27 17:38:40 -0500

    BUILD: JsonParser, FileParser, ParseResult
    Built JsonParser with conversion to text and error
    handling.
    Updated the FileParser interface
    Structured ParseResult

--- src/main/java/com/na/postmortemproject/parser/FileParser.java
+++ src/main/java/com/na/postmortemproject/parser/FileParser.java
@@ -2,6 +2,9 @@ package com.na.postmortemproject.parser;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import com.na.postmortemproject.parser.dto.ParseResult;
+
 public interface FileParser {
-  String extractText(InputStream input) throws IOException;
+  ParseResult parse(InputStream input, String fileName, String fileType, Long fileSize) throws IOException;
 }
